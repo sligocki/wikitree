11f1ae40364406cbab90487d8238329ed0ec89b8
Shawn Ligocki <sligocki@gmail.com>
2019-11-05 13:17:56 -0500

    Count descendants for all ancestors of a person.

--- count_descendants.py
+++ count_descendants.py
@@ -1,26 +1,46 @@
+import collections
 import time
 
 import sqlite_reader
 
 
-def count_descendants(wt_id):
-  db = sqlite_reader.Database()
+def count_descendants(person_num):
   parents = set()
-  parents.add(db.id2num(wt_id))
-  gen_num = 0
-  total_descendants = 0
+  parents.add(person_num)
+  descendants = []
   while parents:
-    total_descendants += len(parents)
-    print gen_num, len(parents), total_descendants, time.clock()
+    descendants.append(len(parents))
     children = set()
     for parent in parents:
       children.update(db.children_of(parent))
-    gen_num += 1
     parents = children
-  print gen_num, len(parents), total_descendants, time.clock()
+  return descendants
+
+
+def descendants_per_ancestor(db, start_num):
+  todo = collections.deque()
+  todo.append((0, 1, start_num))
+  best_count = 0
+  while todo:
+    gen, ahnentafel, person_num = todo.popleft()
+    descendants = count_descendants(person_num)
+    count = sum(descendants)
+    if count > best_count:
+      print gen, ahnentafel, db.num2id(person_num), sum(descendants), descendants
+      best_count = count
+    # Add parents
+    father_num = db.father_of(person_num)
+    if father_num:
+      todo.append((gen + 1, ahnentafel * 2, father_num))
+    mother_num = db.mother_of(person_num)
+    if mother_num:
+      todo.append((gen + 1, ahnentafel * 2 + 1, mother_num))    
 
 
 import sys
 
 wt_id = sys.argv[1]
-count_descendants(wt_id)
+
+db = sqlite_reader.Database()
+start_num = db.id2num(wt_id)
+descendants_per_ancestor(db, start_num)
