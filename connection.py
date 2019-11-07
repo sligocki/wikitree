55f2ad74013cf7ec14d6e04a37d5164200aaa2de
Shawn Ligocki <sligocki@gmail.com>
2019-11-07 16:19:28 -0500

    List muiltiple (equal-length) connections

--- connection.py
+++ connection.py
@@ -1,4 +1,5 @@
 import collections
+import random
 import sys
 import time
 
@@ -8,11 +9,12 @@ import sqlite_reader
 
 
 class Bfs(object):
-  def __init__(self, user_num):
-    self.user_num = user_num
-    # Example shortest path to each other person.
-    self.path = {user_num: ()}
-    self.todo = [user_num]
+  def __init__(self, start):
+    self.start = start
+    self.dists = {start: 0}
+    # Dict { person : [list of neighbors of person on shortest paths to start] }
+    self.paths = {start: []}
+    self.todo = [start]
     self.num_steps = 0
 
   def next_gen(self):
@@ -20,20 +22,41 @@ class Bfs(object):
     self.num_steps += 1
     next_todo = []
     for person in self.todo:
-      path = self.path[person]
+      dist = self.dists[person]
       for neigh in db.neighbors_of(person):
-        if neigh not in self.path:
-          self.path[neigh] = path + (person,)
+        if neigh in self.dists:
+          if self.dists[neigh] == dist + 1:
+            # Found another good path.
+            self.paths[neigh].append(person)
+        else:  # neigh not in self.dists
+          self.dists[neigh] = dist + 1
+          self.paths[neigh] = [person]
           next_todo.append(neigh)
           yield neigh
     self.todo = next_todo
+
+  def get_in_path(self, person):
+    """Get a path from person -> self.start (not including person)."""
+    path = []
+    while person != self.start:
+      # Get one arbitrary path
+      if len(self.paths[person]) > 1:
+        print "Mult options:", person, self.paths[person]
+      person = self.paths[person][0]
+      path.append(person)
+    return path
+
+  def get_out_path(self, person):
+    """Get a path from self.start -> person (not including person)."""
+    return list(reversed(self.get_in_path(person)))
 
 
 def find_connection(person1, person2):
   bfs1 = Bfs(person1)
   bfs2 = Bfs(person2)
 
-  while True:
+  found = False
+  while not found:
     if len(bfs1.todo) <= len(bfs2.todo):
       this = bfs1
       other = bfs2
@@ -42,12 +65,13 @@ def find_connection(person1, person2):
       other = bfs1
 
     for person in this.next_gen():
-      if person in other.path:
-        # Done, we found a common person.
-        # TODO: Find all connections of this length.
-        print "Evaluated %d (%d around %s) & %d (%d around %s)" % (
-          len(bfs1.path), bfs1.num_steps, person1_id, len(bfs2.path), bfs2.num_steps, person2_id)
-        return bfs1.path[person] + (person,) + tuple(reversed(bfs2.path[person]))
+      if person in other.dists:
+        # We found a path
+        found = True
+        yield bfs1.get_out_path(person) + [person] + bfs2.get_in_path(person)
+
+  print "Evaluated %d (%d around %s) & %d (%d around %s)" % (
+    len(bfs1.dists), bfs1.num_steps, person1_id, len(bfs2.dists), bfs2.num_steps, person2_id)
 
 
 person1_id = unicode(sys.argv[1], encoding="utf-8", errors="strict")
@@ -60,8 +84,8 @@ db = sqlite_reader.Database()
 person1 = db.id2num(person1_id)
 person2 = db.id2num(person2_id)
 
-connection = find_connection(person1, person2)
-
-print "Connection from", person1_id, "to", person2_id
-for dist, user_num in enumerate(connection):
-  print dist, db.num2id(user_num), db.name_of(user_num)
+for i, connection in enumerate(find_connection(person1, person2)):
+  print "Connection", i
+  for dist, user_num in enumerate(connection):
+    print "-", dist, db.num2id(user_num), db.name_of(user_num)
+  print
