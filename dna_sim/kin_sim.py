b8d80cc6d0251235d6674569b751a882362d55cb
Shawn Ligocki <sligocki@gmail.com>
2020-01-03 12:04:27 -0500

    Restrict ourselves to only people within a delta of 2 generations (heuristic for people still alive ...)

--- dna_sim/kin_sim.py
+++ dna_sim/kin_sim.py
@@ -12,21 +12,25 @@ import dna_sim
 
 class Person:
   people = []
-  def __init__(self, id, *parents):
+  def __init__(self, id, gen_dist, gen_delta, *parents):
     assert len(parents) in (0, 2), (id, parents)
     self.id = id
+    self.gen_dist = gen_dist
+    self.gen_delta = gen_delta
     self.parents = parents
     self.genome = None
     self.people.append(self)
 
 
-def create_desc(fertility_func, num_gens_down, parent1, parent2=None, remove_child=False):
-  if num_gens_down <= 0:
+def create_desc(fertility_func, max_gen_dist, gen_dist, gen_delta, parent1, parent2=None, remove_child=False):
+  if max_gen_dist == 0:
+    return
+  if gen_delta < -2:
     return
 
   if not parent2:
     # Non-kin spouses
-    parent2 = Person(parent1.id + "S")
+    parent2 = Person(parent1.id + "S", None, gen_delta)
   num_children = fertility_func()
   if remove_child:
     num_children -= 1
@@ -34,27 +38,24 @@ def create_desc(fertility_func, num_gens_down, parent1, parent2=None, remove_chi
     for child_num in range(num_children):
       child_id = parent1.id + "." + str(child_num)
       # TODO: Should we randomize which parent is listed first?
-      child = Person(child_id, parent1, parent2)
-      create_desc(fertility_func, num_gens_down - 1, child)
+      child = Person(child_id, gen_dist, gen_delta, parent1, parent2)
+      create_desc(fertility_func, max_gen_dist - 1, gen_dist + 1, gen_delta - 1, child)
 
 
-def create_tree(fertility_func, num_gens_up, num_gens_down, id_prefix="R"):
+def create_tree(fertility_func, max_gen_dist, gen_dist=0, gen_delta=0, id_prefix="R"):
   """Create a collection of descendants of ancestors.
-
-  Search num_gens_up generations above starting person and then num_gens_down
-  from them using fertility_func() to get # of children.
 
   Assumes no inbreeding.
   """
-  if num_gens_up == 0:
-    return Person(id_prefix)
+  if max_gen_dist == 0:
+    return Person(id_prefix, gen_dist, gen_delta)
 
   parents = []
   for par in ("0", "1"):
-    parents.append(create_tree(fertility_func, num_gens_up - 1, num_gens_down, id_prefix + par))
+    parents.append(create_tree(fertility_func, max_gen_dist - 1, gen_dist + 1, gen_delta + 1, id_prefix + par))
 
-  p = Person(id_prefix, *parents)
-  create_desc(fertility_func, num_gens_down, *parents, remove_child=True)
+  p = Person(id_prefix, gen_dist, gen_delta, *parents)
+  create_desc(fertility_func, max_gen_dist, gen_dist + 1, gen_delta, *parents, remove_child=True)
   return p
 
 
@@ -136,20 +137,31 @@ def summarize_segments(shared_segments):
   print("  Max segment [cM]:", max(sizes, default=0.))
 
 
+def bin_search_index(xs, val):
+  low = 0
+  high = len(xs) - 1
+  while high > low:
+    mid = (low + high) // 2
+    if xs[mid] < val:
+      high = mid - 1
+    else:
+      low = mid + 1
+  return low
+
+
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
-  parser.add_argument("--num-gens-up", type=int)
-  parser.add_argument("--num-gens-down", type=int, default=0)
+  parser.add_argument("--max-gen-dist", type=int)
   args = parser.parse_args()
 
   fertility_func = lambda: 3
-  root = create_tree(fertility_func, args.num_gens_up, args.num_gens_down)
+  root = create_tree(fertility_func, args.max_gen_dist)
 
   # Human autosome (Chromosomes 1-22).
   genome = Genome([281.5, 263.7, 224.2, 214.4, 209.3, 194.1, 187., 169.2, 167.2, 174.1, 161.1, 176., 131.9, 125.2, 132.4, 133.8, 137.3, 129.5, 111.1, 114.8, 70.1, 79.1])
   sim_tree(genome, Person.people)
 
-  shared_amount = []
+  shared_amount = collections.defaultdict(list)
   print("Tree size:", len(Person.people))
   for person in Person.people:
     shared_segments = shared_dna(root.genome, person.genome)
@@ -159,12 +171,12 @@ if __name__ == "__main__":
     # Note: This does not distinguish fully identical vs half-identical regions.
     detected_segments = flatten_segments(shared_segments)
     sizes_detectable = [end-begin for _, begin, end in detected_segments if end-begin >= 7.0]
-    shared_amount.append((sum(sizes_detectable), max(sizes_detectable, default=0.), person.id))
-  print()
-  shared_amount.sort()
-  for total_cm, max_cm, person_id in shared_amount:
-    if person_id[-1] != "S":
-      genetic_dist = len(person_id) - person_id.count(".") - 1
-      if "." in person_id:
-        genetic_dist -= 1
-      print("%3d %-10s %10.2f %10.2f" % (genetic_dist, person_id, total_cm, max_cm))
+    shared_amount[person.gen_dist].append((sum(sizes_detectable), max(sizes_detectable, default=0.), person))
+  print("%3s %8s %8s %8s %8s %8s %8s %8s" % ("Gen", "Max", "90%-ile", "Median", "Min", "Mean", "#>40cM", "#"))
+  for gen_dist in range(args.max_gen_dist + 1):
+    total_cMs = [total_cM for total_cM, _, person in shared_amount[gen_dist] if person.gen_delta <= 2]
+    total_cMs.sort(reverse=True)
+    print("%3d %8.2f %8.2f %8.2f %8.2f %8.2f %8d %8d" % (gen_dist, total_cMs[0], total_cMs[10 * len(total_cMs) // 100], total_cMs[len(total_cMs) // 2], total_cMs[-1], sum(total_cMs) / len(total_cMs), bin_search_index(total_cMs, 40.) + 1, len(total_cMs)))
+  # for total_cm, max_cm, person in shared_amount:
+  #   if person.gen_dist != None:
+  #     print("%3d   %-10s %10.2f %10.2f" % (person.gen_dist, person.id, total_cm, max_cm))
