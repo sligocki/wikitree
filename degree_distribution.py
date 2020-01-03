bd16c3367f9d7a5c23455ee2e29cf0c8c6a03298
Shawn Ligocki <sligocki@gmail.com>
2020-01-03 12:49:11 -0500

    See degree distribution

---
+++ degree_distribution.py
@@ -0,0 +1,22 @@
+
+
+import data_reader
+
+
+db = data_reader.Database()
+db.load_connections()
+
+degrees = []
+total_edges = 0
+for user in db.connections:
+  degree = len(db.connections[user])
+  while len(degrees) - 1 < degree:
+    degrees.append(0)
+  degrees[degree] += 1
+  total_edges += degree  # Note: This actually double counts edges.
+
+print "Number of nodes", len(db.connections)
+print "Number of edges (double counted)", total_edges
+print "Degree distribution:"
+for degree, count in enumerate(degrees):
+  print "Degree", degree, count
