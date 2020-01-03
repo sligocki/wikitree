0a10d6423bff27517976976dcf6bb1e15a5f198c
Shawn Ligocki <sligocki@gmail.com>
2020-01-03 14:19:35 -0500

    Add birth/death dates back to custom_users so that csv_to_sqlite.csv succeeds.

--- rewrite_custom.py
+++ rewrite_custom.py
@@ -1,7 +1,13 @@
 import csv_iterate
 
-fields = ["User ID", "WikiTree ID", "Father", "Mother", "First Name", "Middle Name", "Last Name at Birth", "Last Name Current", "Gender"]
+fields = ["User ID", "WikiTree ID", "Father", "Mother", "First Name", "Middle Name", "Last Name at Birth", "Last Name Current", "Gender", "Birth Date", "Death Date"]
+
+def get_field(user, field):
+  try:
+    return user.row[user.key[field]]
+  except KeyError:
+    return ""
 
 print("\t".join(fields))
 for user in csv_iterate.iterate_users(only_custom=True):
-  print("\t".join(user.row[user.key[field]] for field in fields))
+  print("\t".join(get_field(user, field) for field in fields))
