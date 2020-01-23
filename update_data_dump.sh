20a441d1fe11fd8d027f924e304a47b45235d461
Shawn Ligocki <sligocki@gmail.com>
2020-01-23 14:38:12 -0500

    echo commands so we can see what's happening.

--- update_data_dump.sh
+++ update_data_dump.sh
@@ -7,11 +7,11 @@ USERNAME=${1:-ligocki7}
 
 PW_FILE=$(realpath data/apps.password.txt)
 
-# 1) Check version of data dump.
+echo "(1) Check version of data dump"
 # TODO: Put password in file.
 echo ls -l dumps/dump_people_users.csv.gz | sshpass -f "$PW_FILE" sftp ${USERNAME}@apps.wikitree.com
 
-# 2) Figure out timestamp from ls.
+echo "(2) Figure out timestamp from ls"
 # TODO: Auto-extract timestamp from ls format.
 echo "Input timestamp now: [^C to cancel]"
 read TIMESTAMP
@@ -23,23 +23,27 @@ if [ -d data/dumps/$TIMESTAMP ]; then
   exit 0
 fi
 
-# 3) If we don't yet have this dump, download it.
+echo "(3) If we don't yet have this dump, download it"
 mkdir data/dumps/$TIMESTAMP
 cd data/dumps/$TIMESTAMP
-echo get dumps/* | sshpass -f "$PW_FILE" sftp ${USERNAME}@apps.wikitree.com
+echo get dumps/*.csv.gz | sshpass -f "$PW_FILE" sftp ${USERNAME}@apps.wikitree.com
 
-# 4) Unzip (overwriting previous CSVs)
+echo "(4) Unzip (overwriting previous CSVs)"
 for x in users marriages; do
   gunzip dump_people_${x}.csv.gz -c > ../../dump_people_${x}.csv
 done
 
-# 5) Process new dump
+echo "(5) Process new dump"
 cd ../../..  # Back to main repo
 rm -f data/wikitree_dump.db
+echo csv_to_sqlite.py
 python csv_to_sqlite.py
+echo csv_to_graph.py
 python csv_to_graph.py
+echo csv_to_groups.py
 python csv_to_groups.py
-# TODO: Update sibling-in-laws
+echo "csv_to_groups.py --sibling-in-law"
+python csv_to_groups.py --sibling-in-law
 
-# 6) Print stats about new dump
+echo "(6) Print stats about new dump"
 # TODO
