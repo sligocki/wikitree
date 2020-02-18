90015379210d5ec53dc1175a42c771ebba544e7d
Shawn Ligocki <sligocki@gmail.com>
2020-02-18 15:36:47 -0500

    Plot graph (doesn't help much).

--- cluster_chinese_wispers.py
+++ cluster_chinese_wispers.py
@@ -7,6 +7,7 @@ https://en.wikipedia.org/wiki/Chinese_Whispers_(clustering_method)
 import argparse
 import time
 
+import matplotlib.pyplot as plt
 import networkx as nx
 import chinese_whispers as cw
 
@@ -29,5 +30,11 @@ print("Number of clusters:", len(clusters))
 cluster_sizes = sorted([len(clusters[name]) for name in clusters])
 print("Community sizes:", cluster_sizes[0], "to", cluster_sizes[-1])
 print("Median cluster size:", cluster_sizes[len(clusters) // 2])
+
+print("Plotting graph with clusters", time.process_time())
+label_to_color = {label: i for i, label in enumerate(clusters)}
+colors = [label_to_color[g.nodes[node]['label']] for node in g.nodes()]
+nx.draw(g, cmap=plt.get_cmap('jet'), node_color=colors)
+plt.show()
 
 print(" ... Done", time.process_time())
