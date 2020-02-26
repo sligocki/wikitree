16b7a3b48b7ae72a126243936b3265254ed66475
Shawn Ligocki <sligocki@gmail.com>
2020-02-26 13:53:29 -0500

    Fix bug where we were storing user object instead of user num

--- graph_tools.py
+++ graph_tools.py
@@ -4,4 +4,4 @@ def LargestCombonent(graph):
   max_size, main_component = max(
     ((len(comp), comp) for comp in nx.connected_components(graph)),
     key = lambda x: x[0])
-  main_subgraph = graph.subgraph(main_component)
+  return graph.subgraph(main_component)
--- nuclear_family_graph.py
+++ nuclear_family_graph.py
@@ -21,39 +21,38 @@ import csv_iterate
 import graph_tools
 
 
+def NodeName(parents):
+  return "/".join(str(p) for p in sorted(parents))
+
 print("Loading users", time.process_time())
-# Map node -> people who are members of this node
-members_of = collections.defaultdict(set)
 # Map people -> nodes they are members of
 nodes_of = collections.defaultdict(set)
 for i, person in enumerate(csv_iterate.iterate_users()):
   parents = [p for p in (person.father_num(), person.mother_num()) if p]
   if parents:
-    node = str(sorted(tuple(parents)))
+    node = NodeName(parents)
     # Connect child and parents to this node.
     # Note: Parents will be double added, but it's a set, so that's fine.
-    for member in parents + [person]:
-      members_of[node].add(member)
+    for member in parents + [person.user_num()]:
       nodes_of[member].add(node)
   if i % 1000000 == 0:
-    print(f" ... {i:,}  # Nodes: {len(members_of):,}", time.process_time())
+    print(f" ... {i:,}", time.process_time())
 print("Loading marriages", time.process_time())
 # Connect any spouses. Note: This is redundant for couples with children.
 for marriage in csv_iterate.iterate_marriages():
-  couple = marriages.user_nums()
-  node = str(sorted(tuple(couple)))
+  couple = marriage.user_nums()
+  node = NodeName(couple)
   for member in couple:
-    members_of[node].add(member)
     nodes_of[member].add(node)
 
 print("Building graph", time.process_time())
 graph = nx.Graph()
-for node in members_of:
-  for member in members_of[node]:
-    for neighbor_node in nodes_of[member]:
+for member in nodes_of:
+  for node_a in nodes_of[member]:
+    for node_b in nodes_of[member]:
       # Don't double-add edges
-      if node < neighbor_node:
-        graph.add_edge(node, neighbor_node)
+      if node_a < node_b:
+        graph.add_edge(node_a, node_b)
 print(f"Total graph size: {len(graph.nodes):,} Nodes {len(graph.edges):,} Edges.")
 
 print("Writing graph to file", time.process_time())
