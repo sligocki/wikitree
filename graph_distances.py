f10a4fb45d9c0586d08c3fe2e291c08e9d2c07e9
Shawn Ligocki <sligocki@gmail.com>
2020-02-27 23:45:18 -0500

    sqlite is hard :/

--- graph_distances.py
+++ graph_distances.py
@@ -16,7 +16,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("graph")
 args = parser.parse_args()
 
-conn = sqlite3.connect("data/distances.db")
+conn = sqlite3.connect("data/distances.db", timeout=20)
 conn.row_factory = sqlite3.Row
 cursor = conn.cursor()
 cursor.execute("CREATE TABLE IF NOT EXISTS distances (graph STRING, node STRING, mean_dist REAL)")
@@ -25,13 +25,20 @@ print("Loading graph", time.process_time())
 graph = nx.read_adjlist(args.graph)
 print(f"Initial graph:  # Nodes: {len(graph.nodes):,}  # Edges: {len(graph.edges):,}", time.process_time())
 
-for i in itertools.count():
-  print("Computing single point centrality", i, time.process_time())
-  node = random.choice(list(graph.nodes))
-  centrality = nx.closeness_centrality(graph, u=node)
-  mean_dist = 1./centrality
-  print(f"Centrality {node} {centrality:.4f} {mean_dist:.2f}")
+nodes = list(graph.nodes)
+random.shuffle(nodes)
 
-  print("Saving to DB", time.process_time())
-  cursor.execute("INSERT INTO distances VALUES (?, ?, ?)",
-                 (args.graph, node, mean_dist))
+for i, node in enumerate(nodes):
+  try:
+    print("Node", i, node, time.process_time())
+    centrality = nx.closeness_centrality(graph, u=node)
+    mean_dist = 1./centrality
+    print(f"Centrality\t{node}\t{centrality:.4f}\t{mean_dist:.2f}")
+
+    print("Saving to DB", time.process_time())
+    cursor.execute("INSERT INTO distances VALUES (?, ?, ?)",
+                   (args.graph, node, mean_dist))
+    if i % 10 == 0:
+      conn.commit()
+  except sqlite3.OperationalError:
+    print(" !!! SQLite error !!!")
