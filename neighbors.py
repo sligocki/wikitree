5ee7bf619a3dceb97b94d1eafc21ac8fc1b39cd4
Shawn Ligocki <sligocki@gmail.com>
2020-03-03 13:42:52 -0500

    Sort neighbors by relationship and put in columns for readability.

--- neighbors.py
+++ neighbors.py
@@ -6,9 +6,15 @@ import data_reader
 
 db = data_reader.Database()
 
-for wikitree_id in sys.argv[1:]:
-  user_num = db.id2num(wikitree_id)
-  print("Neighbors of:", db.name_of(user_num), wikitree_id, user_num)
-  for neigh in db.neighbors_of(user_num):
-    print("-", db.relationship_type(user_num, neigh), db.name_of(neigh), db.num2id(neigh), neigh)
+for id_or_num in sys.argv[1:]:
+  try:
+    user_num = int(id_or_num)
+  except ValueError:
+    user_num = db.id2num(id_or_num)
+  print("Neighbors of:", db.name_of(user_num), db.num2id(user_num), user_num)
+  neighbors = [(db.relationship_type(user_num, neigh), neigh)
+               for neigh in db.neighbors_of(user_num)]
+  neighbors.sort()
+  for rel_type, neigh in neighbors:
+    print(f" - {rel_type:10} {db.name_of(neigh):30} {db.num2id(neigh):20} {neigh:10}")
   print()
