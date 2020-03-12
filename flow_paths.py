1491b35f86d88b4397544340501f36d8309aff1e
Shawn Ligocki <sligocki@gmail.com>
2020-03-11 21:30:09 -0400

    Switch flow_paths over to use graphs instead of csv directly

--- flow_paths.py
+++ flow_paths.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """
 Look over all shortest paths to every other profile. Find the number of these
 paths which go through each of our relatives. This can give a sense for how
@@ -16,11 +17,12 @@ import sys
 import time
 
 from graphviz import Digraph
+import networkx as nx
 
 import data_reader
 
 
-def flow_paths(db, start):
+def flow_paths(graph, start):
   # Map from person -> shortest distance to start. Also used as a visited set
   # to determine if we have found this node yet.
   dists = {start: 0}
@@ -38,7 +40,7 @@ def flow_paths(db, start):
     person = queue.popleft()
     dist = dists[person]
     cur_sources = sources[person]
-    for neigh in db.neighbors_of(person):
+    for neigh in graph.neighbors(person):
       if neigh not in dists:
         # Found a shortest path.
         dists[neigh] = dist + 1
@@ -68,9 +70,9 @@ def flow_paths(db, start):
   return {person: float(len(on_path_to[person])) / len(all_connections)
           for person in sources}, sources, dists
 
-def create_dot(db, start, flows, sources, cuttoff):
-  """Create a graphviz DOT file with all people who have flow >= cuttoff and all of their neighbors."""
-  dot = Digraph(name=("results/Flows_%s_%.2f" % (db.num2id(start), cuttoff)))
+def create_dot(graph, start, flows, sources, cutoff, naming_func):
+  """Create a graphviz DOT file with all people who have flow >= cutoff and all of their neighbors."""
+  dot = Digraph(name=("results/Flows_%s_%.2f" % (naming_func(start), cutoff)))
 
   todo = collections.deque()
   todo.append(start)
@@ -81,36 +83,51 @@ def create_dot(db, start, flows, sources, cuttoff):
       continue
     visited.add(person)
 
-    # TODO: Add readable name.
-    person_label = db.num2id(person)
+    person_label = naming_func(person)
     dot.node(str(person), label=person_label)
     for source in sources[person]:
-      # TODO: Add weights.
       dot.edge(str(person), str(source), label="%.2f" % flows[person])
 
-    for neigh in db.neighbors_of(person):
-      if flows[neigh] >= cuttoff:
+    for neigh in graph.neighbors(person):
+      if flows[neigh] >= cutoff:
         todo.append(neigh)
 
   dot.view()
 
-if __name__ == "__main__":
+def try_decode_wikitree_id(db, node):
+  parts = []
+  for part in node.split("/"):
+    try:
+      part = db.num2id(int(part))
+    except:
+      pass
+    parts.append(part)
+  return "/".join(parts)
+
+def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument("user_id", nargs="+")
-  parser.add_argument("--cuttoff", type=float, default=0.05,
+  parser.add_argument("nodes", nargs="+")
+  parser.add_argument("--graph", default="data/connection_graph.main.adj.nx")
+  parser.add_argument("--cutoff", type=float, default=0.05,
                       help="Cuttoff for including connection in DOT.")
   args = parser.parse_args()
 
   db = data_reader.Database()
-  db.load_connections()
+  print("Loading graph", args.graph, time.process_time())
+  graph = nx.read_adjlist(args.graph)
 
-  for user_id in args.user_id:
-    print("Analyzing", user_id, time.process_time())
-    start = db.id2num(user_id)
-    flows, sources, dists = flow_paths(db, start)
+  for node in args.nodes:
+    # If a wikitree_id is passed in, translate it to a user_num.
+    try:
+      node = str(db.id2num(node))
+    except:
+      pass
+    print("Analyzing", node, time.process_time())
+    flows, sources, dists = flow_paths(graph, node)
 
     print("Creating DOT", time.process_time())
-    create_dot(db, start, flows, sources, cuttoff=args.cuttoff)
+    create_dot(graph, node, flows, sources, cutoff=args.cutoff,
+               naming_func = lambda node: try_decode_wikitree_id(db, node))
 
     print("Ordering people", time.process_time())
     # Order folks from most flow to least.
@@ -121,9 +138,13 @@ if __name__ == "__main__":
     max_dist = -1
     for frac, person in ordered_people:
       if dists[person] > max_dist:
-        print(user_id, dists[person], frac, db.name_of(person), db.num2id(person))
+        print(node, dists[person], frac, try_decode_wikitree_id(db, person))
         max_dist = dists[person]
         if max_dist >= 20:
           break
 
   print("Done", time.process_time())
+
+
+if __name__ == "__main__":
+  main()
