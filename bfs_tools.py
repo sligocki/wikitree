5066e0514aa877b837eb7cfa7954771f5e458903
Shawn Ligocki <sligocki@gmail.com>
2020-08-19 12:27:27 -0400

    Pull out general BFS enumeration into separate tool. Allow searching for notable relatives.

---
+++ bfs_tools.py
@@ -0,0 +1,45 @@
+"""
+Breadth-first search tools for enumerating connections or relatives of a person.
+"""
+import collections
+
+import data_reader
+
+
+def ConnectionBfs(db, start):
+  todos = collections.deque()
+  dists = {}
+
+  todos.append(start)
+  dists[start] = 0
+
+  while todos:
+    person = todos.popleft()
+    dist = dists[person]
+    yield (person, dist)
+    for neigh in db.neighbors_of(person):
+      if neigh not in dists:
+        todos.append(neigh)
+        dists[neigh] = dist + 1
+
+
+def RelativeBfs(db, start):
+  todos = collections.deque()
+  dists = {}
+
+  todos.append(start)
+  dists[start] = (0, 0)
+
+  while todos:
+    person = todos.popleft()
+    (dist_up, dist_down) = dists[person]
+    yield (person, (dist_up, dist_down))
+    if dist_down == 0:
+      for parent in db.parents_of(person):
+        if parent not in dists:
+          todos.append(parent)
+          dists[parent] = (dist_up + 1, dist_down)
+    for child in db.children_of(person):
+      if child not in dists:
+        todos.append(child)
+        dists[child] = (dist_up, dist_down + 1)
--- notable_connections.py
+++ notable_connections.py
@@ -4,33 +4,23 @@ Find closest connected people in the "Notables" or any other group.
 
 import argparse
 import collections
+import itertools
 
+import bfs_tools
 import category_tools
 import data_reader
 
 
-def list_distances(db, start, targets, max_num):
-  dists = {start: 0}
-  todo = collections.deque()
-  todo.append(start)
-
-  num_printed = 0
-  while todo:
-    person = todo.popleft()
-    dist = dists[person]
+def EnumConnections(bfs, targets):
+  for (person, dist) in bfs:
     if person in targets:
-      print(dist, db.num2id(person), db.name_of(person))
-      num_printed += 1
-      if num_printed >= max_num:
-        return
-    for neigh in db.neighbors_of(person):
-      if neigh not in dists:
-        todo.append(neigh)
-        dists[neigh] = dist + 1
+      yield (person, dist)
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("person_id", help="Person to search from.")
+parser.add_argument("--relatives", action="store_true",
+                    help="Find notable blood relatives (instead of all connections).")
 parser.add_argument("--category", default="Notables",
                     help="Category name to search for connections to.")
 parser.add_argument("--max-num", type=int, default=20,
@@ -41,4 +31,10 @@ db = data_reader.Database()
 start = db.id2num(args.person_id)
 
 targets = category_tools.list_category(args.category)
-list_distances(db, start, targets, args.max_num)
+if args.relatives:
+  bfs = bfs_tools.RelativeBfs(db, start)
+else:
+  bfs = bfs_tools.ConnectionBfs(db, start)
+
+for (person, dist) in itertools.islice(EnumConnections(bfs, targets), args.max_num):
+  print(dist, db.num2id(person), db.name_of(person))
