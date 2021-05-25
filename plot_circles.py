3232ad01247b04973c9cf24da480b78f637ac5b1
Shawn Ligocki <sligocki@gmail.com>
2021-05-24 22:44:54 -0400

    Allow plotting log-normal regression.

--- plot_circles.py
+++ plot_circles.py
@@ -2,6 +2,7 @@
 
 import argparse
 import json
+import math
 from pathlib import Path
 
 import matplotlib.pyplot as plt
@@ -21,10 +22,14 @@ def median_index(circle_sizes):
 parser = argparse.ArgumentParser()
 parser.add_argument("circles_json")
 parser.add_argument("wikitree_ids", nargs="*")
+
 parser.add_argument("--log-y", action="store_true")
 parser.add_argument("--rate", action="store_true")
 parser.add_argument("--relative", action="store_true",
                     help="Rescale distances relative to median dist.")
+parser.add_argument("--log-normal-regression", action="store_true",
+                    help="Plot a log-normal distribution regression")
+
 parser.add_argument("--save-image", type=Path,
                     help="Instead of displaying plot, save it to a file.")
 args = parser.parse_args()
@@ -51,6 +56,7 @@ else:
 
 if args.log_y:
   ax.set_yscale("log")
+  ax.set_ylim(1, 100_000_000)
 else:
   # matplotlib does not allow plain format for log axis ...
   ax.ticklabel_format(style="plain")
@@ -74,6 +80,34 @@ for wikitree_id in ids:
     ys = [ys[i+1] / ys[i] for i in range(len(ys) - 1)]
 
   ax.plot(xs, ys, label=wikitree_id)
+
+  if args.log_normal_regression:
+    count = 0
+    total = 0
+    total2 = 0
+    for dist, this_count in enumerate(sizes):
+      if dist > 0:
+        count += this_count
+        total += math.log(dist) * this_count
+        total2 += math.log(dist)**2 * this_count
+
+    mean = total / count
+    stddev = math.sqrt(total2 / count - mean**2)
+
+    print("Plotting with regression", mean, stddev)
+    # Log-normal only works for positive values!
+    xs = [x for x in xs if x > 0]
+    # Log-normal PDF
+    # See https://en.wikipedia.org/wiki/Log-normal_distribution#Probability_density_function
+    ys = [count / (x * stddev * math.sqrt(2 * math.pi)) *
+          math.e**(-(math.log(x) - mean)**2 / (2 * stddev**2))
+          for x in xs]
+
+    if args.rate:
+      del xs[0]
+      ys = [ys[i+1] / ys[i] for i in range(len(ys) - 1)]
+
+    ax.plot(xs, ys, label=f"Regression_{wikitree_id}")
 
 ax.legend()
 fig.set_size_inches(8, 8)
