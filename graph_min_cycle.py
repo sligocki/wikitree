e4e3674890a118ce99af175e551a529247418293
Shawn Ligocki <sligocki@gmail.com>
2024-01-10 16:49:52 -0500

    Update graph_min_cycle to support min-cycles around an edge as well as around a node.

--- graph_min_cycle.py
+++ graph_min_cycle.py
@@ -1,5 +1,5 @@
 """
-Find minimal cycles through a given node in graph.
+Find minimal cycles through a given node or edge in graph.
 """
 
 import argparse
@@ -13,39 +13,65 @@ import graph_tools
 import utils
 
 
-def min_cycle(graph, start_node):
-  # Map node -> path
-  paths = {start_node: []}
-  todos = []
+def edge(u, v):
+  return tuple(sorted((u, v)))
 
-  # Start us off with neighbors of start_node (the path directions).
-  for dir in graph.adj[start_node]:
-    paths[dir] = [dir]
-    todos.append(dir)
-  dirs_seen = set(todos)
+def path_from_parents(parents, node):
+  path = []
+  while node:
+    path.append(node)
+    node = parents[node]
+  return path
 
-  while todos and len(dirs_seen) > 1:
+def min_path(graph, colors, ignore_edges):
+  """Find smallest path in `graph` which connects two nodes of different color.
+  Colors are spread via adjacency (ignoring nodes already in `colors`)."""
+  parents = {n: None for n in colors}
+  todos = list(colors.keys())
+  colors_seen = set(colors.values())
+  while todos and len(colors_seen) > 1:
     new_todos = []
-    dirs_seen = set()
+    # All colors used in this layer. We keep track to allow early exit if all
+    # but one color die out.
+    colors_seen = set()
     for node in todos:
-      path = paths[node]
       for neigh in graph.adj[node]:
-        if neigh in paths:
-          # Found a cycle.
-          path_neigh = paths[neigh]
-          if path_neigh and path[0] != path_neigh[0]:
-            # The cycle goes through start_node.
-            return [start_node] + path + list(reversed(path_neigh))
+        if edge(node, neigh) not in ignore_edges:
+          if neigh not in colors:
+            parents[neigh] = node
+            colors[neigh] = colors[node]
+            colors_seen.add(colors[node])
+            new_todos.append(neigh)
 
-        else:  # neigh not yet visited
-          paths[neigh] = path + [neigh]
-          new_todos.append(neigh)
-          dirs_seen.add(path[0])
+          elif colors[node] != colors[neigh]:
+            # We found a cycle!
+            return list(reversed(path_from_parents(parents, node))) + path_from_parents(parents, neigh)
 
     todos = new_todos
 
   # No cycle
   return None
+
+def min_cycle_edge(graph, e):
+  """Find smallest cycle in `graph` containing edge `e`."""
+  u, v = e
+  colors = {u: u, v: v}
+  return min_path(graph, colors, ignore_edges = {e})
+
+def min_cycle_node(graph, start_node):
+  """Find smallest cycle in `graph` containing node `start_node`."""
+  colors = {}
+  ignore_edges = set()
+  for neigh in graph.adj[start_node]:
+    colors[neigh] = neigh
+    ignore_edges.add(edge(start_node, neigh))
+
+  path = min_path(graph, colors, ignore_edges)
+  if path:
+    return [start_node] + path
+  else:
+    return None
+
 
 def check_geodesic(graph, cycle):
   """Is this a geodesic cycle? Meaning, is this a cycle where, for any two nodes on the cycle, the shortest path remains on the cycle."""
@@ -77,29 +103,43 @@ def main():
   if args.nodes:
     for node in args.nodes:
       utils.log("Searching for cycle through", node)
-      cycle = min_cycle(graph, node)
-      utils.log("Min cycle", node, len(cycle), cycle)
+      cycle = min_cycle_node(graph, node)
+      if cycle:
+        utils.log("Min cycle", node, len(cycle), cycle)
+      else:
+        utils.log("No cylces through", node)
 
   else:
-    nodes = list(graph.nodes)
-    random.shuffle(nodes)
+    # If no nodes are specified, look for all min cycles.
+    # We search over all edges for a more complete view.
+    # We can safely ignore edges with both nodes degree 2 because those are
+    # redundant with results from other edges.
+    edges = list((u, v) for (u, v) in graph.edges
+                 if graph.degree[u] > 2 or graph.degree[v] > 2)
+    random.shuffle(edges)
+    utils.log(f"Searching through {len(edges):_} edges")
     max_min_cycle = -1
     cycle_lengths = collections.Counter()
-    for i, node in enumerate(nodes):
+    for i, e in enumerate(edges):
       if i % 1_000 == 0:
-        utils.log("Checkpoint:", i, " ".join(f"{n}:{cycle_lengths[n]:_}" for n in sorted(cycle_lengths.keys())))
+        utils.log(f"Checkpoint: {i:_}",
+                  " ".join(f"{n}:{cycle_lengths[n]:_}"
+                           for n in sorted(cycle_lengths.keys())))
 
-      cycle = min_cycle(graph, node)
+      cycle = min_cycle_edge(graph, e)
       if not cycle:
         cycle_lengths[0] += 1
+        utils.log("No cycles", i, e)
       else:
         cycle_lengths[len(cycle)] += 1
         if len(cycle) > max_min_cycle:
-          utils.log("Cycle", i, node, len(cycle), cycle[:6])
+          utils.log("Cycle", i, e, len(cycle), cycle[:6])
           max_min_cycle = len(cycle)
         elif len(cycle) >= 100:
-          utils.log("Cycle", i, node, len(cycle), cycle[:6])
+          utils.log("Cycle", i, e, len(cycle), cycle[:6])
 
+    utils.log("Final:", i, " ".join(f"{n}:{cycle_lengths[n]:_}"
+                                    for n in sorted(cycle_lengths.keys())))
   utils.log("Done")
 
 if __name__ == "__main__":
