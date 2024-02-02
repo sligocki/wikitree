faed04a7557224616eb24a8dbd3f09335aed90b9
Shawn Ligocki <sligocki@gmail.com>
2024-02-02 16:21:13 -0500

    Add option to pre-load connection DB.

--- circles_count.py
+++ circles_count.py
@@ -21,15 +21,18 @@ def main():
   parser.add_argument("focus_id", nargs="+")
   parser.add_argument("--num-circles", "-n", type=int, default=7)
   parser.add_argument("--list", "-l", action="store_true")
+  parser.add_argument("--load-connections", "--load", action="store_true")
   parser.add_argument("--version", help="Data version (defaults to most recent).")
   args = parser.parse_args()
 
   db = data_reader.Database(args.version)
+  if args.load_connections:
+    db.load_connections()
 
   for id in args.focus_id:
     circles = circles_tools.load_circles(db, id, args.num_circles)
     sizes = [len(circle) for circle in circles]
-    print(id, ":", sum(sizes), ":", sizes)
+    print(f"{id:15s} : {sum(sizes):7d} :", sizes)
 
     if args.list:
       for dist in range(args.num_circles + 1):
@@ -38,3 +41,6 @@ def main():
           done = db.connections_complete_of(person_num)
           done_str = " " if done else "X"
           print(f"   * {i:4} {done_str} {try_id(db, person_num)}")
+
+if __name__ == "__main__":
+  main()
