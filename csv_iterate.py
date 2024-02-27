b5c685c9fb809871e754bae1765231ace2f9c128
Shawn Ligocki <sligocki@gmail.com>
2024-02-27 00:30:39 -0500

    mypy csv_iterate.py

--- csv_iterate.py
+++ csv_iterate.py
@@ -1,6 +1,7 @@
 import csv
 import datetime
 from pathlib import Path
+from typing import Iterator
 
 import utils
 
@@ -13,13 +14,13 @@ def LoadMin(s):
   return max(num, 1)
 
 
-def ParseInt(int_str):
+def ParseInt(int_str : str) -> int | None:
   if not int_str:
     return None
   return int(int_str)
 
 
-def ParseBool(bool_str):
+def ParseBool(bool_str : str) -> bool | None:
   if bool_str == "1":
     return True
   elif bool_str == "0":
@@ -28,7 +29,7 @@ def ParseBool(bool_str):
     return None
 
 
-def ParseDate(date_str):
+def ParseDate(date_str : str) -> datetime.date | None:
   if not date_str or date_str == "0":
     return None
   year = int(date_str[:4])
@@ -42,7 +43,7 @@ def ParseDate(date_str):
     raise
 
 
-def ParseTimestamp(timestamp_str):
+def ParseTimestamp(timestamp_str : str) -> datetime.datetime | None:
   if not timestamp_str or timestamp_str == "0":
     return None
   year = int(timestamp_str[:4])
@@ -57,20 +58,20 @@ def ParseTimestamp(timestamp_str):
     print(timestamp_str, year, month, day, hour, min, sec)
     raise
 
-def StringOrNone(str):
-  if str:
-    return str
+def StringOrNone(s : str) -> str | None:
+  if s:
+    return s
   else:
     # If string is empty, return None.
     return None
 
 
 class Row:
-  def __init__(self, row, key):
+  def __init__(self, row, key) -> None:
     self.row = row
     self.key = key
 
-  def lookup(self, col_name):
+  def lookup(self, col_name : str):
     try:
       return self.row[self.key[col_name]]
     except (IndexError, KeyError):
@@ -81,16 +82,16 @@ class UserRow(Row):
   def user_num(self):
     return ParseInt(self.lookup("User ID"))
 
-  def wikitree_id(self):
+  def wikitree_id(self) -> str:
     return self.lookup("WikiTree ID_DB")
 
-  def father_num(self):
+  def father_num(self) -> int | None:
     return ParseInt(self.lookup("Father"))
 
-  def mother_num(self):
+  def mother_num(self) -> int | None:
     return ParseInt(self.lookup("Mother"))
 
-  def birth_name(self):
+  def birth_name(self) -> str:
     first_name = self.lookup("First Name")
     if not first_name:
       first_name = self.lookup("Preferred Name")
@@ -98,43 +99,43 @@ class UserRow(Row):
       first_name = "(Unlisted)"
     return first_name + " " + self.lookup("Last Name at Birth")
 
-  def gender_code(self):
+  def gender_code(self) -> int | None:
     # Note: Gender is stored as 0 = Blank, 1 = Male, 2 = Female.
     # Also possible is "" which might mean private info?
     return ParseInt(self.lookup("Gender"))
 
-  def birth_date(self):
+  def birth_date(self) -> datetime.date | None:
     return ParseDate(self.lookup("Birth Date"))
 
-  def death_date(self):
+  def death_date(self) -> datetime.date | None:
     return ParseDate(self.lookup("Death Date"))
 
-  def birth_location(self):
+  def birth_location(self) -> str | None:
     return StringOrNone(self.lookup("Birth Location"))
 
-  def death_location(self):
+  def death_location(self) -> str | None:
     return StringOrNone(self.lookup("Death Location"))
 
-  def no_more_children(self):
+  def no_more_children(self) -> bool | None:
     return ParseBool(self.lookup("No Children"))
 
-  def no_more_siblings(self):
+  def no_more_siblings(self) -> bool | None:
     return ParseBool(self.lookup("No Children"))
 
   # TODO: Doesn't exist yet.
-  # def no_more_spouses(self):
+  # def no_more_spouses(self) -> bool | None:
   #   return ParseBool(self.lookup("No Spouses"))
 
-  def registered_time(self):
+  def registered_time(self) -> datetime.datetime | None:
     return ParseTimestamp(self.lookup("Registration"))
 
-  def touched_time(self):
+  def touched_time(self) -> datetime.datetime | None:
     return ParseTimestamp(self.lookup("Touched"))
 
-  def edit_count(self):
+  def edit_count(self) -> int | None:
     return ParseInt(self.lookup("Edit Count"))
 
-  def privacy_level(self):
+  def privacy_level(self) -> int | None:
     """ Levels:
         "UNLISTED": 10,
         "PRIVATE": 20,
@@ -146,11 +147,11 @@ class UserRow(Row):
     """
     return ParseInt(self.lookup("Privacy"))
 
-  def manager_num(self):
+  def manager_num(self) -> int | None:
     return ParseInt(self.lookup("Manager"))
 
 
-def iterate_users_file(filename):
+def iterate_users_file(filename : Path) -> Iterator[UserRow]:
   with open(filename, "r") as f:
     reader = csv.reader(f, delimiter='\t', quoting=csv.QUOTE_NONE)
 
@@ -166,10 +167,10 @@ def iterate_users_file(filename):
       yield UserRow(row, key)
 
 
-def iterate_users(*, version, only_custom=False):
-  custom_user_nums = set()
+def iterate_users(*, version : str, only_custom : bool = False) -> Iterator[UserRow]:
+  custom_user_nums : set[int] = set()
   # Note: We don't use version dir for custom_users. Just use global one.
-  for user in iterate_users_file("data/custom_users.csv"):
+  for user in iterate_users_file(Path("data/custom_users.csv")):
     custom_user_nums.add(user.user_num())
     yield user
 
@@ -182,18 +183,18 @@ def iterate_users(*, version, only_custom=False):
 
 
 class MarriageRow(Row):
-  def user_nums(self):
+  def user_nums(self) -> list[int]:
     return [int(self.lookup("User ID1")),
             int(self.lookup("UserID2"))]
 
-  def marriage_date(self):
+  def marriage_date(self) -> datetime.date | None:
     return ParseDate(self.lookup("Marriage Date"))
 
-  def marriage_location(self):
+  def marriage_location(self) -> str:
     return self.lookup("Marriage Location")
 
 
-def iterate_marriages_file(filename):
+def iterate_marriages_file(filename : Path) -> Iterator[MarriageRow]:
   with open(filename, "r") as f:
     reader = csv.reader(f, delimiter='\t', quoting=csv.QUOTE_NONE)
 
@@ -209,13 +210,13 @@ def iterate_marriages_file(filename):
       yield MarriageRow(row, key)
 
 
-def iterate_marriages(*, version, only_custom=False):
+def iterate_marriages(*, version : str, only_custom : bool = False) -> Iterator[MarriageRow]:
   if not only_custom:
     for marriage in iterate_marriages_file(Path(utils.data_version_dir(version),
                                                 "dump_people_marriages.csv")):
       yield marriage
   # Note: We don't use version dir for custom_users. Just use global one.
-  for marriage in iterate_marriages_file("data/custom_marriages.csv"):
+  for marriage in iterate_marriages_file(Path("data/custom_marriages.csv")):
     yield marriage
 
 
@@ -225,7 +226,7 @@ if __name__ == "__main__":
 
   id = sys.argv[1]
   i = 0
-  for user in iterate_users():
+  for user in iterate_users(version = "default"):
     i += 1
     if (i % 1000000) == 0:
       print("... Read", i, "records", time.process_time())
